f14816a2ce519733e14771a8dc363c76b0aafefb
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-03-20 19:58:04 -0300

    configure standalone routes

--- AngularInsights/src/app/app.routes.ts
+++ AngularInsights/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    title: 'Angular Insights - Home'
+  },
+  {
+    path: 'posts',
+    loadComponent: () => import('./pages/post-list/post-list.component').then(m => m.PostListComponent),
+    title: 'Artigos Técnicos'
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () => import('./pages/post-detail/post-detail.component').then(m => m.PostDetailComponent),
+    title: 'Detalhes do Artigo'
+  }
+];
